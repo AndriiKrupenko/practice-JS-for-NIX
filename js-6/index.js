aa60fbb71a7a306c84e641d9b572017e6ca948cd
AndriiKrupenko <krupenkoandrei1991@gmail.com>
2021-08-12 22:30:00 +0300

    12.08.2021(2) commit

--- js-6/index.js
+++ js-6/index.js
@@ -1,3 +1,15 @@
-let 
+let arr = [10, 20, 30, 50, 235, 3000];
+
+let out_num = document.getElementById('out_num');
+
+let p = ' ';
+
+for (let num of arr) {
+  let numArr = (''+num)[0];
+    if (numArr == 1 || numArr == 2 || numArr == 5) {
+        p = p + num + ' ';
+        out_num.innerHTML = p;
+    }
+}
 
 
